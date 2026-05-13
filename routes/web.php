981a2febb62a4d8f0eb84c3d4a653aa7be321f90
Andres Alexandro Aviña Zaragoza <aavina966@gmail.com>
2026-05-13 12:13:56 -0600

    fix: Remove 'verified' middleware from routes for consistency in authentication

--- routes/web.php
+++ routes/web.php
@@ -17,39 +17,39 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('decant/generar', [DecantController::class, 'generarDecant'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('decant.generar');
 
 Route::post('decant/rellenar', [DecantController::class, 'rellenar'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('decant.rellenar');
 
 Route::get('/ventas/historial', [VentaController::class, 'historial'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('venta.historial');
 
 Route::get('/pedidos/detallePedidos', [PedidosController::class, 'detallePedidos'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('pedidos.detallePedidos');
 
-Route::put('/pedidos/estado/{id}', [PedidosController::class, 'estado'])->name('pedidos.estado')->middleware(['auth', 'verified']);
+Route::put('/pedidos/estado/{id}', [PedidosController::class, 'estado'])->name('pedidos.estado')->middleware(['auth']);
 
 
 Route::get('/pedidos/edit/{id}', [PedidosController::class, 'edit'])->name('pedidos.edit');
 
-Route::resource('dashboard', dashboardController::class)->middleware(['auth', 'verified']);
-Route::resource('pedidos', PedidosController::class)->middleware(['auth', 'verified']);
-Route::resource('inventario_decants', InventarioDecantController::class)->middleware(['auth', 'verified']);
-Route::resource('decant', DecantController::class)->middleware(['auth', 'verified']);
-Route::resource('abonos', RegistrarAbonoController::class)->middleware(['auth', 'verified']);
-Route::resource('marca', MarcaController::class)->middleware(['auth', 'verified']);
-Route::resource('usuario', UsuarioController::class)->middleware(['auth', 'verified']);
-Route::resource('perfume', PerfumeController::class)->middleware(['auth', 'verified']);
-Route::resource('proovedor', ProovedorController::class)->middleware(['auth', 'verified']);
-Route::resource('cliente', ClienteController::class)->middleware(['auth', 'verified']);
-Route::resource('inventario', InventarioController::class)->middleware(['auth', 'verified']);
-Route::resource('venta', VentaController::class)->middleware(['auth', 'verified']);
-Route::resource('deuda', DeudaController::class)->middleware(['auth', 'verified']);
+Route::resource('dashboard', dashboardController::class)->middleware(['auth']);
+Route::resource('pedidos', PedidosController::class)->middleware(['auth']);
+Route::resource('inventario_decants', InventarioDecantController::class)->middleware(['auth']);
+Route::resource('decant', DecantController::class)->middleware(['auth']);
+Route::resource('abonos', RegistrarAbonoController::class)->middleware(['auth']);
+Route::resource('marca', MarcaController::class)->middleware(['auth']);
+Route::resource('usuario', UsuarioController::class)->middleware(['auth']);
+Route::resource('perfume', PerfumeController::class)->middleware(['auth']);
+Route::resource('proovedor', ProovedorController::class)->middleware(['auth']);
+Route::resource('cliente', ClienteController::class)->middleware(['auth']);
+Route::resource('inventario', InventarioController::class)->middleware(['auth']);
+Route::resource('venta', VentaController::class)->middleware(['auth']);
+Route::resource('deuda', DeudaController::class)->middleware(['auth']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
